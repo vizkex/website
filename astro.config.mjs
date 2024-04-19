ab0e6c81ca6c08c1d6dda6831a331a3be61ebf86
vizkex <vizkex@gmail.com>
2024-04-19 09:05:33 +0300

    editing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify(),
+  edgeMiddleware: true
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Home';
+export const blog_TITLE = 'blog';
+export const about_TITLE = 'about';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
